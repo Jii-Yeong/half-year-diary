e6627cf7091f7f3b0cf42031a059a9767013b19c
Nam Eungyeong <nek1717@naver.com>
2021-09-17 17:49:17 +0900

    로그인, 회원가입 페이지 프로젝트 링크 연결

--- client/src/components/LogoBanner.js
+++ client/src/components/LogoBanner.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import logoBanner from "../assets/images/logo_banner.jpg";
+import githubImg from "../assets/images/github.png";
 
 const Banner = styled.article`
+  position: relative;
   width: 50%;
   height: 100%;
   background-image: url(${logoBanner});
@@ -12,7 +14,17 @@ const Banner = styled.article`
 `;
 
 const LogoBanner = () => {
-  return <Banner />;
+  return (
+    <Banner>
+      <a
+        href="https://github.com/yeongyeng/half-year-diary"
+        className="move-project"
+        target="_block"
+      >
+        <img src={githubImg} alt="깃허브 이동" />
+      </a>
+    </Banner>
+  );
 };
 
 export default LogoBanner;
